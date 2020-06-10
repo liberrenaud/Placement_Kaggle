a89bf6a29f6f723ec47b89596dd1c06b7120c5bc
Renaud Liber <liberrenaud@gmail.com>
2020-06-10 16:17:34 +0200

    Initial commit
    High level EDA - most common Spec + Sankey chart on what leads to placement

---
+++ 00_Functions/Create_Folder_structure.R
@@ -0,0 +1,24 @@
+library(fs)
+
+
+
+
+make_project_dir <- function() {
+  
+  dir_names <- c(
+    "00_Data",
+    "00_Functions",
+    "01_Business_Understanding",
+    "02_Data_Understanding",
+    "03_Data_Preparation",
+    "04_Modeling",
+    "05_Evaluation",
+    "06_Deployment")
+  
+  dir_create(dir_names)
+  
+  dir_ls()
+}
+  
+
+make_project_dir()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Placement_Kaggle
+
+Note that data can be downloaded at following location : https://www.kaggle.com/benroshan/factors-affecting-campus-placement?select=Placement_Data_Full_Class.csv
